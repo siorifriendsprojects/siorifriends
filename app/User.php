e19c66d899c9f4c20287795825e008ce54c6dd6e
Alucky4423 <a.lucky2510@gmail.com>
2017-07-31 12:27:18 +0900

    update : User Model 作成
    default であったUser Model とmigration file に
    uuid と account を追加した。

--- app/User.php
+++ app/User.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Alsofronie\Uuid\Uuid32ModelTrait;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use Uuid32ModelTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'account', 'name', 'email', 'password',
     ];
 
     /**
@@ -26,4 +28,18 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    protected $incrementing = false;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    protected $timestamps   = false;
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->char('id', 32)->primary();
+            $table->string('account')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+//            $table->timestamps();
         });
     }
 
