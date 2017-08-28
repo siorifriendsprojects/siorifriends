c45ed0ce87cfdedf6384739573af35beadb43531
Alucky4423 <a.lucky2510@gmail.com>
2017-08-28 13:53:50 +0900

    add : anchor table.

---
+++ database/migrations/2017_08_28_044300_create_anchors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnchorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anchors', function (Blueprint $table) {
+            $table->char('id', 32)->primary();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anchors');
+    }
+}
