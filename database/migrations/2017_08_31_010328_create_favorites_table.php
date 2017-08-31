5093815453b17083ff4368f6ba3990d4a29ef328
Alucky4423 <a.lucky2510@gmail.com>
2017-08-31 10:10:28 +0900

    add : favorite table.

---
+++ database/migrations/2017_08_31_010328_create_favorites_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->char('id', 32)->primary();
+            $table->char('user_id', 32);
+            $table->char('book_id', 32);
+//            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
