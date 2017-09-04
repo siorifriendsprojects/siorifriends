d096b03526ffcc613db2331da3a5e245c24badde
as-is-prog <as-is-prog@outlook.jp>
2017-09-04 10:48:22 +0900

    [fix] "/{id}"以下のルーティングが原因で/homeが機能しなかったのを修正

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+/*
+ここのまとまりより下には/{id}/から始まらないルーティングを追加しないでください。
+理由->/{id}/のルーティングが優先されて機能しないため
+*/
 Route::get('/{id}','UserController@show');
 
 Route::get('/{id}/follow','UserController@showFollow');
 
 Route::get('/{id}/follower','UserController@showFollower');
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
