14806c2c6b344865112ad8e3210e53251344567d
monchi <tyun0113@gmail.com>
2017-09-21 12:21:17 +0900

    labelの文字を日本語へ変更

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -6,19 +6,19 @@
     <form class="form-horizontal">
 
         <div class="form-group">
-            <label for="title">Title:</label>
+            <label for="title">タイトル:</label>
             <input type="text" id="title" class="form-control">
         </div>
         <div class="form-group">
-            <label for="description">Overview:</label>
+            <label for="description">概要:</label>
             <textarea id="description" class="form-control"></textarea>
         </div>
         <div class="form-group">
-            <label for="tag">Tag:</label>
+            <label for="tag">タグ:</label>
             <input type="text" id="tag" class="form-control">
         </div>
         <div class="form-group">
-            <label for="Adults">Adults Only:</label>
+            <label for="Adults">１８歳未満への公開:</label>
             <input type="checkbox"  data-toggle="toggle" data-on="Enabled" data-off="Disabled">
         </div>
         <div class="form-group">
