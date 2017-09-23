f66b467aa7340d3f8bee1f364bc9b6467f63fd6e
Alucky4423 <a.lucky2510@gmail.com>
2017-09-24 02:05:00 +0900

    [update] follow, follower の両方を表示できるように変更した。

--- resources/views/follow.blade.php
+++ resources/views/follow.blade.php
@@ -1,27 +1,32 @@
 @extends('layouts.template')
 
 @section('content')
-        <button class="btn btn-link btn-lg btn-block back-btn">
-            <span class="glyphicon glyphicon-arrow-left"></span>
-            フォロー
-        </button>
-        @foreach ($user->followUsers as $followUser)
+    <button class="btn btn-link btn-lg btn-block back-btn">
+        <span class="glyphicon glyphicon-arrow-left"></span>
+        フォロー
+    </button>
+
+    @foreach ($users as $user)
         <div class="row list-padding-border">
             <div class="col-xs-3 col-xs-offset-1 list-padding">
                 <img src="/img/四葉栞_顔.png" alt="しおり" class="img-circle img-responsive img-icon" />
             </div>
-            <div class="col-xs-3 list-padding"><span>{{ $followUser->name }}</span>
+            <div class="col-xs-3 list-padding"><span>{{ $user->name }}</span>
                 <div class="row">
-                    <div class="col-xs-12"><span><a href="/{{ $followUser->account }}">{{ '@' . $followUser->account }}</a></span></div>
+                    <div class="col-xs-12"><span><a href="/{{ $user->account }}">{{ '@' . $user->account }}</a></span></div>
                 </div>
             </div>
-            <div class="col-xs-5 list-padding">
-            @if( Auth::user()->isFollow($followUser->id) )
-                <button class="btn btn-success center-block">フォロー中</button>
-            @else
-                <button class="btn btn-primary center-block">フォロー</button>
+
+            @if( Auth::check() )
+                <div class="col-xs-5 list-padding">
+                @if( Auth::user()->isFollow($user->id) )
+                    <button class="btn btn-success center-block">フォロー中</button>
+                @else
+                    <button class="btn btn-primary center-block">フォロー</button>
+                @endif
+                </div>
             @endif
-            </div>
+
         </div>
-        @endforeach
+    @endforeach
 @endsection
