67c501e11a0ce898ae4bec41a17c6d18f5e8d290
Alucky4423 <a.lucky2510@gmail.com>
2017-09-28 14:54:18 +0900

    [update] tag, comment 関連のテストメソッドを追加した。

--- tests/Unit/EloquentBookTest.php
+++ tests/Unit/EloquentBookTest.php
@@ -4,6 +4,9 @@ namespace Tests\Unit;
 
 use App\SioriFriends\Models\Book\Anchor;
 use App\SioriFriends\Models\Book\Book;
+use App\SioriFriends\Models\Book\Comment;
+use App\SioriFriends\Models\Book\Tag;
+use App\SioriFriends\Models\User\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -16,7 +19,11 @@ class EloquentBookTest extends TestCase
     /** @var Book */
     private $target;
 
+    /** @var Anchor */
     private $anchor;
+
+    /** @var Tag */
+    private $tag;
 
     protected function setUp()
     {
@@ -24,6 +31,7 @@ class EloquentBookTest extends TestCase
 
         $this->target = factory(Book::class)->create();
         $this->anchor = factory(Anchor::class)->create();
+        $this->tag = factory(Tag::class)->create();
     }
 
     public function testAddAnchor()
@@ -44,6 +52,42 @@ class EloquentBookTest extends TestCase
         $this->assertDatabaseMissing('anchor_book', [
             'anchor_id' => $this->anchor->id,
             'book_id' => $this->target->id,
+        ]);
+    }
+
+    public function testAddTag()
+    {
+        $this->target->addTag($this->tag);
+
+        $this->assertDatabaseHas('book_tag', [
+            'book_id' => $this->target->id,
+            'tag_id' => $this->tag->id,
+        ]);
+    }
+
+    public function testRemoveTag()
+    {
+        $this->testAddTag();
+        $this->target->removeTag($this->tag);
+
+        $this->assertDatabaseMissing('book_tag', [
+            'book_id' => $this->target->id,
+            'tag_id' => $this->tag->id,
+        ]);
+    }
+
+    public function testAddComment()
+    {
+        \Faker\Provider\ja_JP\Text::class;
+        $comment = \Faker\Factory::create('ja_JP')->text();
+        $user = factory(User::class)->create();
+
+        $this->target->addComment($user, $comment);
+
+        $this->assertDatabaseHas('comments', [
+            'user_id' => $user->id,
+            'book_id' => $this->target->id,
+            'body' => $comment,
         ]);
     }
 }
