93407bc1c9c0c7a6dfe11b2a3d5378eb01bf771f
Alucky4423 <a.lucky2510@gmail.com>
2017-10-02 14:32:53 +0900

    [update] method を追加した。

--- app/SioriFriends/Models/Book/BookRepository.php
+++ app/SioriFriends/Models/Book/BookRepository.php
@@ -9,7 +9,17 @@
 namespace App\SioriFriends\Models\Book;
 
 
+use Illuminate\Database\Eloquent\Model;
+
 interface BookRepository
 {
+    public function findAll();
 
+    public function findById(string $bookId);
+
+    public function add(Model $book);
+
+    public function save(Model $book);
+
+    public function remove(Model $book);
 }
