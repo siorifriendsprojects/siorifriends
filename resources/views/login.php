b48ed595b0b8e410e5f5b97b75dc554152759c97
monchi <tyun0113@gmail.com>
2017-10-16 16:22:33 +0900

    login-bladeの作成

---
+++ resources/views/login.php
@@ -0,0 +1,26 @@
+<div class="container">
+          <div class="col-xs-10 col-xs-offset-1 ">
+             <img src="img/logo01.png" alt="タイトルロゴ" class="img-responsive"></img>
+          </div>
+
+	<div class="row">
+		<div class="col-xs-10	 col-xs-offset-1  ">
+			<form action='' method=''  class="in">
+				<p><input class="form-control" type='text' name='' placeholder="userID"></p>
+				<p><input class="form-control" type='password' name=''placeholder="password"></p>
+        <p><label><input type="checkbox"><small>ログイン状態を維持</small></Label></p>
+				<p><input  class="btn btn-success btn-block" type='submit' value='ログイン'></p>
+			</form>
+         <div class="text-lowercase">外部ログイン</div>
+			<div class="row well well-sm">
+				  <div class="btn-group-xs  col-xs-offset-1 " role="group " >
+					 <button class="btn btn-facebook"><i class="facebook"></i><a href="/login/facebook_request_auth">Facebook</a></button>
+					 <button class="btn btn-twitter"><i class="twitter"></i><a href="">twitter</a></button>
+					 <button class="btn btn-LINE"><i class="LINE"></i><a href ="https://access.line.me/dialog/oauth/weblogin  ">LINE</a></button>
+					 <button class="btn btn-Google"><i class="Google"></i><a href="">Google</a></button>
+          </div>
+      </div>
+       <p><div class="col-xs-7 col-xs-offset-3"><a href="">新規登録はこちら</a></div></p>
+		</div>
+	</div>
+</div>
