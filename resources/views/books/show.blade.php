9862bd7114a83d5661b6afaf2df24c84bc78a8ec
Alucky4423 <a.lucky2510@gmail.com>
2017-11-27 16:09:45 +0900

    [update] 本の削除機能を実装した。
    `books/show.blade.php`
    
    - 削除ボタンを追加した
    - 編集ボタンを追加した
    
    `Controllers/BookController.php`
    
    - show method に本の編集、削除ボタンを表示するための条件式を追加した
    - destroy method を実装した。

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.template')
 
 @section('content')
-    <div class="col-xs-12 container">
+<div class="container">
+    <div class="row">
         <div class="col-xs-12">
             <div class="h4 user">著者名 : {{ $book->author->account}}</div>
             <h3 class="lines-empty text-center">{{ $book->title }}</h3>
@@ -13,22 +14,59 @@
 
             <div class="page text-left">
                 <ul class="list-group">
-                    <h3>
                     @foreach($book->anchors as $anchor)
                         <li class="list-group-item"><a href="{{ $anchor->url }}">{{ $anchor->pivot->name }}</a></li>
                     @endforeach
-                    </h3>
                 </ul>
-            </div>
-            <div class="comment">
-                <div class="comm-li col-xs-offset-1">
-                </div>
-                <div class="form-group">
-                    <label for="comment-lav" class="left-block control-label">comment</label>
-                    <input type="text" id="input-comm" class="form-control">
-                    <button class="btn btn-primary lines-btn pull-right" id="comm-btn">投稿</button>
-                </div>
             </div>
         </div>
     </div>
+    {{-- ログイン状態、かつ、自分の本の場合に表示する --}}
+    @if ($isMyBook)
+        <div class="row">
+            <div class="col-xs-1 col-xs-offset-3">
+                {{--<button id="editButton" type="button" class="btn btn-primary">編集</button>--}}
+                <a href="{{ route('books.edit', ['bookId' => $book->id]) }}" class="btn btn-primary" role="button">編集</a>
+            </div>
+
+            {{-- 本を削除するためのフォーム --}}
+            <div class="col-xs-1 col-xs-offset-3">
+                <form class="col" action="{{ route('books.destroy', ['bookId' => $book->id]) }}" id="deleteForm" method="post">
+                    {{ method_field('delete') }}
+                    {{ csrf_field() }}
+                    <button type="button" id="deleteButton" class="btn btn-danger">削除</button>
+                </form>
+            </div>
+        </div>
+    @endif
+
+    <div class="comment">
+        <div class="comm-li col-xs-offset-1"></div>
+        <div class="form-group">
+            <label for="comment-lav" class="left-block control-label">comment</label>
+            <input type="text" id="input-comm" class="form-control">
+            <button class="btn btn-primary lines-btn pull-right" id="comm-btn">投稿</button>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('resources')
+<script>
+$(function() {
+  // 必要な html 要素のセレクタ
+  const Selector = {
+    deleteButton: '#deleteButton',
+    deleteForm  : '#deleteForm',
+  };
+
+  // 削除ボタンを押した時に確認フォームを表示する
+  $(Selector.deleteButton).on('click', function() {
+    const confirmMsg = '本を削除します\nよろしいですか?';
+    // 確認画面を表示して OK が押されたら、form を送信する
+    const ok = confirm(confirmMsg);
+    if (ok) $(Selector.deleteForm).submit();
+  });
+});
+</script>
 @endsection
