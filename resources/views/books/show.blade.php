94d0d9ed922893190266dbd95282bffa8d59e34d
Alucky4423 <a.lucky2510@gmail.com>
2017-12-03 00:52:00 +0900

    [update] いくつかレイアウトを変更、追加した。
    - 変更内容
      - 編集、削除ボタンをDropdowns に変更した
      - Grid layout の修正
      - tag の表示

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -3,45 +3,80 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-xs-12">
-            <div class="h4 user">著者名 : {{ $book->author->account}}</div>
-            <h3 class="lines-empty text-center">{{ $book->title }}</h3>
-            <h6 class="create-date text-center">作成日 : {{ $book->created_at->format('Y-m-d H:i')}}</h6>
+        <div class="col-xs-5">
+            <p>
+                <a href="{{ route('overview', ['account' => $book->author->account]) }}">{{ $book->author->account}}</a>
+            </p>
+        </div>
 
+        <div class="col-xs-5">
+            <p>作成日 : {{ $book->created_at->format('Y-m-d H:i') }}</p>
+        </div>
+
+        {{-- ログイン状態、かつ、自分の本の場合に表示する --}}
+        @if ($isMyBook)
+            {{-- menu button --}}
+            <div class="col-xs-2 text-right">
+                <div class="dropdown">
+                    <button id="dropdownMenuButton" class="btn btn-default dropdown-toggle"
+                            type="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                        <span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuBtn">
+                        <li><a href="{{ route('books.edit', ['bookId' => $book->id]) }}">編集</a></li>
+                        <li class="alert-danger">
+                            <a id="deleteButton" class="alert-link" href="#">削除
+                                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        @endif
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>{{ $book->title }}</h1>
+        </div>
+    </div>
+
+    {{-- tag の一覧を表示 --}}
+    <div class="row">
+        <div class="col-xs-12">
+            <p class="h4">
+                @foreach($book->tags as $tag)
+                    <span class="label label-default">{{ $tag->name }}</span>
+                @endforeach
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
             <div class="lines-empty text-center">
                 {{ $book->description }}
             </div>
+        </div>
+    </div>
 
-            <div class="page text-left">
-                {{-- linkの一覧を表示 --}}
-                <div class="list-group">
-                    @foreach($book->anchors as $anchor)
-                        <a href="{{ $anchor->url }}" class="list-group-item list-group-item-action" target="_blank" style="color: #03a9f4">
-                            {{ $anchor->pivot->name }}
-                        </a>
-                    @endforeach
-                </div>
+    {{-- linkの一覧を表示 --}}
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="list-group">
+                @foreach($book->anchors as $anchor)
+                    <a href="{{ $anchor->url }}"
+                       class="list-group-item list-group-item-action"
+                       target="_blank"
+                       style="color: #03a9f4">
+                        {{ $anchor->pivot->name }}
+                    </a>
+                @endforeach
             </div>
         </div>
     </div>
-    {{-- ログイン状態、かつ、自分の本の場合に表示する --}}
-    @if ($isMyBook)
-        <div class="row">
-            <div class="col-xs-1 col-xs-offset-3">
-                {{--<button id="editButton" type="button" class="btn btn-primary">編集</button>--}}
-                <a href="{{ route('books.edit', ['bookId' => $book->id]) }}" class="btn btn-primary" role="button">編集</a>
-            </div>
 
-            {{-- 本を削除するためのフォーム --}}
-            <div class="col-xs-1 col-xs-offset-3">
-                <form id="deleteForm" action="{{ route('books.destroy', ['bookId' => $book->id]) }}" method="post">
-                    {{ method_field('delete') }}
-                    {{ csrf_field() }}
-                    <button type="button" id="deleteButton" class="btn btn-danger">削除</button>
-                </form>
-            </div>
-        </div>
-    @endif
 
     <div class="comment">
         <div class="comm-li col-xs-offset-1"></div>
@@ -63,14 +98,23 @@ $(function() {
     deleteForm  : '#deleteForm',
   };
 
+  const deleteFormElements = `
+    <form id="deleteForm" action="{{ route('books.destroy', ['bookId' => $book->id]) }}" method="post">
+        {{ method_field('delete') }}
+        {{ csrf_field() }}
+    </form>
+  `;
+
   // 削除ボタンを押した時に確認フォームを表示する
   $(Selector.deleteButton).on('click', function() {
-    $(this).prop('disabled', true);
     const confirmMsg = '本を削除します\nよろしいですか?';
     // 確認画面を表示して OK が押されたら、form を送信する
     const ok = confirm(confirmMsg);
-    if (ok) $(Selector.deleteForm).submit();
-    $(this).prop('disabled', false);
+    if (ok) {
+        const deleteForm = $(deleteFormElements);
+        $(document.body).append(deleteForm);
+        deleteForm.submit();
+    }
   });
 });
 </script>
