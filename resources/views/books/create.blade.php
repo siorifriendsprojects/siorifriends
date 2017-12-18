ac83615b70fceeda71e1f48f2a18923f560ee915
Alucky4423 <a.lucky2510@gmail.com>
2017-12-18 15:02:45 +0900

    [update] create.blade の見栄えを変更した。

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -3,129 +3,242 @@
 @section('content')
 
 <div class="container">
-    <div class='col-xs-offset-1 col-xs-10' id='input-item'>
-        <form action={{ route('books.store') }} method="post" class="form-horizontal col-xs-offset-1 col-xs-10">
-            {{ csrf_field() }}
-                <!-- タイトルのグループ -->
-            <div class="form-group">
-                <label for="title">タイトル:</label>
-                <input type="text" name="title" id="title" class="form-control">
-            </div>
-                <!-- 概要のグループ -->
-            <div class="form-group">
-                <label for="description">概要:</label>
-                <textarea id="description" name="description" class="form-control" style="resize : none;"></textarea>
-            </div>
-                <!-- URL追加ページのグループ  -->
-            <div class="form-group" >
-                <label for="addURL">リンクの追加:</label>
-                <div class="url-group">
-                    <div class="lines-empty cnt" >
-                        <label for="title" class="h6">リンクタイトル</label>
-                        <input type="text" class="form-control cnt-title">
-                        <label for="title" class="h6">URL</label>
-                        <input type="text" class="form-control cnt-url">
-                    </div>
-                </div>
-                <button type="button" class="btn btn-default btn-circle pull-right lines-empty" id="addcnt"><i class="glyphicon glyphicon-plus" ></i></button>
-            </div>
-                <!-- タグのグループ -->
-
-            <div class="form-group taggroup">   
-                <label for="tag">タグ:</label>
-                <input type="text" id="tags" class="form-control" />
-            </div>
-                <!-- 全体へ公開するか -->
-            <div class="form-group text-center">
-                <p class="pull-left"><label for="Comment">全体公開設定:</label></p>
-                <div class="btn-group text-center publishing" data-toggle="buttons">
-                    <label class="btn btn-default active">
-                        <input type="radio" name="is_publishing" autocomplete="off" value=true checked>公開する
-                    </label>
-                    <label class="btn btn-default">
-                        <input type="radio" name="is_publishing" autocomplete="off" value=false>公開しない
-                    </label>
-                </div>
-            </div>
-                <!-- １８歳未満の公開設定 -->
-            <div class="form-group text-center">
-                <p class="pull-left"><label for="Adults">１８歳未満公開設定:</label> </p>
-                <div class="btn-group text-center adult" data-toggle="buttons">
-                    <label class="btn btn-default active">
-                        <input type="radio" name="is_adult" autocomplete="off" value=true checked> 公開する
-                    </label>
-                    <label class="btn btn-default">
-                        <input type="radio" name="is_adult" autocomplete="off" value=false> 公開しない
-                    </label>
-                </div>
-            </div>
-                <!-- コメントの許可設定 -->
-            <div class="form-group text-center">
-                <p class="pull-left"><label for="Comment">コメントの許可設定:</label></p>
-                <div class="btn-group text-center commentable" data-toggle="buttons">
-                    <label class="btn btn-default active">
-                        <input type="radio" name="is_commentable" autocomplete="off" value=true checked> 許可する
-                    </label>
-                    <label class="btn btn-default">
-                        <input type="radio" name="is_commentable" autocomplete="off" value=false> 許可しない
-                    </label>
-                </div>
-            </div>
-        </form>
-                <!-- 確認ボタン -->
-        <div class="text-center col-xs-12">
-            <button class="btn btn-default" id="confirmation">確認</button>
+    {{--.row*2>.col-xs-12>.row*2>.col-xs-12--}}
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>本作成フォーム</h1>
         </div>
     </div>
-    <!-- ここから確認画面  -->
-    <div class="col-xs-offset-1 col-xs-10" id='check' style='display:none'>
-        <div class="lines-empty col-xs-12">
-            <label class="check-cnt">タイトル</label>
-            <div class="col-xs-offset-1 col-xs-11 check-lav" id="check-title"></div>
-        </div>
-        <div class="lines-empty col-xs-12">
-            <label class="check-cnt">概要</label>
-            <div class="col-xs-offset-1 col-xs-11 check-lav" id="check-description"></div>
-        </div>
-        
-        <div class="lines-empty col-xs-12">
-            <p class="check-cnt"><strong>リンク</strong></p>
-                <table class="table third">
-                    <tr class="check-link"></tr>
-                </table>
-        </div>
-        
-        <div class="lines-empty col-xs-12">
-            <label class="check-cnt">タグ</label>
-            <div class="col-xs-offset-1 col-xs-11 check-tag"></div>
-        </div>
 
-        <label class="check-cnt">公開設定</label>
-        <div class="is-settings col-xs-offset-1 lines-empty">
-            <div class="lines-empty">
-                <label class="check-cnt" name="publishing">全体公開</label>
-                <p class="check-lav" id="publishing"></p>
-            </div>
-            <div class="lines-empty">
-                <label class="check-cnt" name="adult">１８歳未満公開</label>
-                <p class="check-lav" id="adult"></p>
-            </div>
-            <div class="lines-empty">
-                <label class="check-cnt" name="commentable">コメントの許可設定</label>
-                <p class="check-lav" id="comment"></p>
-            </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <input id="title" type="text" class="form-control" placeholder="タイトル">
         </div>
+    </div>
 
-        <div class="text-center">
-        <div style="display:inline-flex">
-                <button class="btn btn-default" id="revision">修正</button>
-                <button class="btn btn-default" id="create">作成</button>
+    <div class="row">
+        <div class="col-xs-12">
+            <textarea id="description" class="form-control" rows="3" placeholder="概要" style="resize: vertical"></textarea>
         </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <h3>タグ</h3>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <select id="tags" class="form-control" multiple data-role="tagsinput"></select>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <h3>リンク</h3>
+        </div>
+    </div>
+
+    <div class="row">
+        <div id="linkList" class="col-xs-12">
+
+            {{-- url title 一つ分 --}}
+            {{--<div class="row" data-type="link">--}}
+                {{--<div class="col-xs-12 form-group">--}}
+                    {{--<div class="input-group">--}}
+                        {{--<input type="text" class="form-control" placeholder="url" data-type="linkUrl">--}}
+                        {{--<div class="input-group-btn">--}}
+                            {{--<button class="btn btn-info active" data-button="preview">--}}
+                                {{--<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>--}}
+                            {{--</button>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<input type="text" class="form-control" placeholder="title" data-type="linkTitle">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 text-center">
+            <button id="addLinkFieldBtn" class="btn btn-default btn-lg">
+                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+            </button>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <h3>オプション</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 text-center">
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>外部に公開するかどうか</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <input id="isPublished" type="checkbox" class="form-control" checked
+                           data-toggle="toggle"
+                           data-onstyle="primary" data-offstyle="danger"
+                           data-on="public" data-off="private"
+                           data-width="100">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 text-center checkbox form-group">
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>コメント</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <input id="isCommentable" type="checkbox" class="form-control" checked
+                           data-toggle="toggle"
+                           data-onstyle="primary" data-offstyle="danger"
+                           data-on="許可" data-off="禁止"
+                           data-width="100">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-xs-12 text-center">
+            <button id="confirmBtn" type="button" class="btn btn-primary btn-lg btn-block">確認する</button>
         </div>
     </div>
 </div>
 @endsection
 
 @section('resources')
-    <script src="{{ asset('js/create.js') }} "></script>
+<script>
+const Book = {
+  title: '#title',
+  description: '#description',
+  tags: '#tags',
+  isPublished: '#isPublished',
+  isCommentable: '#isCommentable',
+};
+
+const Trigger = {
+  previewButtons: '[data-button=preview]',
+  confirmButton: '#confirmBtn',
+  addLinkFieldButton: '#addLinkFieldBtn',
+};
+
+class Link {
+   static get ELEMENT() {
+     return `
+    <div class="row" data-type="link">
+        <div class="col-xs-12 form-group">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="url" data-type="linkUrl">
+                <div class="input-group-btn">
+                    <button class="btn btn-info active" data-button="preview">
+                        <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                    </button>
+                </div>
+            </div>
+            <input type="text" class="form-control" placeholder="title" data-type="linkTitle">
+        </div>
+    </div>
+    `;
+  }
+
+  static get PreviewCallback() {
+    return (e) => {
+      const url = $(e.target).parents('[data-type=link]').find('[data-type=linkUrl]').val();
+      window.open(url, '__blank');
+    };
+  }
+
+  static get Selector() {
+    return {
+      previewButton: '[data-button=preview]',
+      url: '[data-type=linkUrl]',
+      title: '[data-type=linkTitle]',
+    };
+  }
+
+  constructor() {
+    this.jqueryObject = $(Link.ELEMENT);
+
+    // click したら新しいタブを開く
+    this.jqueryObject
+        .find(Link.Selector.previewButton)
+        .on('click', Link.PreviewCallback);
+  }
+
+  url() {
+    return this.jqueryObject.find(Link.Selector.url).val();
+  }
+
+  title() {
+    return this.jqueryObject.find(Link.Selector.title).val();
+  }
+
+  render() {
+    return this.jqueryObject;
+  }
+}
+
+/**
+ * 初期化処理
+ */
+function initialize() {
+  // tagの上限を設定
+  $(Book.tags).tagsinput({
+    maxTags: 10,
+  });
+
+  // link field のひとつ目を追加
+  const link = new Link();
+  $('#linkList').append(link.render());
+
+
+  const initalState = {
+    links: [link],
+  };
+
+  return initalState;
+}
+
+
+/**
+ * main
+ */
+$(() => {
+  let state = initialize();
+
+  $(Trigger.addLinkFieldButton).on('click', (e) => {
+    const link = new Link();
+    state.links = [...state.links, link];
+    $('#linkList').append(link.render());
+  });
+
+  $(Trigger.confirmButton).on('click', (e) => {
+    const results = {
+      title: $(Book.title).val(),
+      description: $(Book.description).val(),
+      tags: $(Book.tags).val(),
+      isPublished: $(Book.isPublished).prop('checked'),
+      isCommentable: $(Book.isCommentable).prop('checked'),
+      links: state.links.map(link => [link.url(), link.title()]),
+    };
+    console.log(results);
+  });
+});
+
+</script>
+    {{--<script src="{{ asset('js/create.js') }} "></script>--}}
 @endsection
